e844fe8888f27065d619db27b05abc1726a69000
Srikrishna Das <krish@tribily.com>
2011-04-17 12:43:55 +0530

    finished adding error handles and responding to user argument

--- App_MySQL/script/mysql_repl_status.pl
+++ App_MySQL/script/mysql_repl_status.pl
@@ -7,20 +7,28 @@ use strict;
 use DBI;
 use DBD::mysql;
 
+
+# Receive Arguments or display Usage
+if (@ARGV < 1  || @ARGV > 1) {
+        print "Usage: mysql_repl_status.pl CHECK\n";
+        print "CHECK can be 'iorun' - Slave IO Running, 'sqlrun' - Slave SQL Running, 'secbmaster' - Seconds behind Master\n";
+        exit;
+}
+
 # VARIABLES
 #
-my $host = "192.168.1.6";
+my $host = "localhost";
 my $port = "3306";
-my $user = "root";
-my $pass = "iforgotit";
-my $dsn = "dbi:mysql:zabbix:$host:$port";
+my $user = "tribilyagent";
+my $pass = "1f0rgOtit";
+my $dsn = "dbi:mysql::$host:$port";
 #$ENV{DBI_AUTOPROXY} = "host=$host;port=$port";
 
 # DBI Connect
 my $dbh = DBI->connect($dsn,$user,$pass,{ PrintError=>0, RaiseError=>1 }) || die "Error Connecting to Database: $DBI::errstr\n";
 
 # Query
-my $sth = $dbh->prepare("show databases;") || die "Query preparing failed: $DBI::errstr\n";
+my $sth = $dbh->prepare("SHOW SLAVE STATUS;") || die "Query preparing failed: $DBI::errstr\n";
 
 # Execute
 $sth->execute() || die "Failed to execute query: $DBI::errstr\n";
@@ -29,13 +37,26 @@ $sth->execute() || die "Failed to execute query: $DBI::errstr\n";
 my $result = $sth->fetchrow_hashref() || die "Failed to fetch data\n";
 
 # Print all Values
-while (my ($key, $val) = each %$result) {
-	print "$key = $val\n";
+#while (my ($key, $val) = each %$result) {
+#        print "$key = $val\n";
+#}
+# Return values based on argument.
+#
+if ( $ARGV[0] eq "iorun" ) {
+        print $result->{'Slave_IO_Running'}."\n";
 }
-
+elsif ( $ARGV[0] eq "sqlrun" ) {
+        print $result->{'Slave_SQL_Running'}."\n";
+}
+elsif ( $ARGV[0] eq "secbmaster" ) {
+        print $result->{'Seconds_Behind_Master'}."\n";
+}
+else {
+        print "Usage: mysql_repl_status.pl CHECK\n";
+        print "CHECK can be 'iorun' - Slave IO Running, 'sqlrun' - Slave SQL Running, 'secbmaster' - Seconds behind Master\n";
+}
 
 
 # DBI Disconnect
 $sth->finish();
 $dbh->disconnect || die "Failed to disconnect\n";
-

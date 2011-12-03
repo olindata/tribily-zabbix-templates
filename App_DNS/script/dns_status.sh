507483df9bf026677057d604c82397e0b049516a
Srikrishna Das <krish@tribily.com>
2011-12-04 01:11:56 +0530

    adding dns script old

--- App_DNS/script/dns_status.sh
+++ App_DNS/script/dns_status.sh
@@ -12,21 +12,48 @@ else
 	DNS_SERVER=$1
 fi
 
+
 SITES=("tribily.com" "olindata.com" "oplexing.com")
 
+		
 for site in ${SITES[*]}
-do
-	if [ `$LOOKUP -s -W $WAIT_TIME $site $DNS_SERVER | grep 'has address' | wc -l` -eq 0 ]
+	do
+		if [ `$LOOKUP -s -W $WAIT_TIME $site $DNS_SERVER | grep 'has address' | wc -l` -eq 0 ]
+		then
+			EXITSUM=`expr $EXITSUM + 0`
+		else
+			EXITSUM=`expr $EXITSUM + 1`
+		fi
+	done
+	
+	if [ $EXITSUM -gt 0 ]
 	then
-		EXITSUM=`expr $EXITSUM + 0`
+		echo 1
 	else
-		EXITSUM=`expr $EXITSUM + 1`
+		echo 0
 	fi
-done
 
-if [ $EXITSUM -gt 0 ]
-then
-	echo 1
-else
-	echo 0
-fi
+
+#if [ -f "$1" ]
+#then
+#	DNS_LIST=$1
+#	for DNS_SERVER in `cat ${$DNS_LIST}`
+#	do
+#		for site in ${SITES[*]}
+#		do
+#			if [ `$LOOKUP -s -W $WAIT_TIME $site $DNS_SERVER | grep 'has address' | wc -l` -eq 0 ]
+#			then
+#				EXITSUM=`expr $EXITSUM + 0`
+#			else
+#				EXITSUM=`expr $EXITSUM + 1`
+#			fi
+#		done
+#		
+#		if [ $EXITSUM -gt 0 ]
+#		then
+#			echo 1
+#		else
+#			echo 0
+#		fi
+#	done
+#fi

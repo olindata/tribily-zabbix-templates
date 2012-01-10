1dc2907c623aac1715a02479480ad05f10577dff
Srikrishna Das <krish@tribily.com>
2012-01-10 17:41:35 +0530

    adding script versioning to dns status script

--- App_DNS/script/dns_status.sh
+++ App_DNS/script/dns_status.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+# Script to monitor dns query - 1 means working, 0 is not.
+# Author: krish@tribily.com
+# Organization: Tribily
+# Version: 1.1
 #
-# Tribily script to monitor dns query - 1 means working, 0 is not.
-#
+VERSION=1.1
+
+# Variables
+ZBX_SENDER=`which zabbix_sender`
+ZBX_CONF="/etc/zabbix/zabbix_agentd.conf"
+
+function zsend { 
+  $ZBX_SENDER -c $ZBX_CONF -k $1 -o $2
+}
+
+
 LOOKUP=`which host`
 WAIT_TIME="5"
 EXITSUM=0
@@ -60,3 +73,7 @@ fi
 #		fi
 #	done
 #fi
+
+
+zsend tribily.dns.ver `echo ${VERSION}`
+

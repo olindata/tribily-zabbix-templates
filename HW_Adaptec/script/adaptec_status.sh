5bb56ae852aa78aafb959bfe26bfdb5a3c9dd514
Srikrishna Das <krish@tribily.com>
2012-01-18 18:35:20 +0530

    Updated template 1.1 and script - must upgrade for all users.

--- HW_Adaptec/script/adaptec_status.sh
+++ HW_Adaptec/script/adaptec_status.sh
@@ -9,95 +9,97 @@
 ARCBIN=`which arcconf`
 VERSION=1.1
 
+
 # Functions to return Adaptec stats
 
 # Controller Info
 function adpt.conmod {
-	$ARCBIN getconfig 1 | grep "Controller Model" | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g'
+  sudo $ARCBIN getconfig 1 | grep "Controller Model" | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g'
 }
 
 # Temperature
 function adpt.temp {
-	$ARCBIN getconfig 1 | grep "Temperature" | cut -f2 -d":" | cut -f1 -d"C"
+  sudo $ARCBIN getconfig 1 | grep "Temperature" | cut -f2 -d":" | cut -f1 -d"C" | sed 's/ //' | xargs echo
 }
 
 # Available Logical Devices
 function adpt.ldevon {
-	$ARCBIN getconfig 1 | grep "Logical devices\/Failed\/Degraded" | cut -f2 -d":" | cut -f1 -d"/"
+  sudo $ARCBIN getconfig 1 | grep "Logical devices\/Failed\/Degraded" | cut -f2 -d":" | cut -f1 -d"/" | sed 's/ //'
 }
 
 # Failed Logical Devices
 function adpt.ldevfail {
-	$ARCBIN getconfig 1 | grep "Logical devices\/Failed\/Degraded" | cut -f2 -d":" | cut -f2 -d"/"
+  sudo $ARCBIN getconfig 1 | grep "Logical devices\/Failed\/Degraded" | cut -f2 -d":" | cut -f2 -d"/"
 }
 
 # Degraded Logical Devices
 function adpt.ldevdeg {
-	$ARCBIN getconfig 1 | grep "Logical devices\/Failed\/Degraded" | cut -f2 -d":" | cut -f3 -d"/"
+  #sudo $ARCBIN getconfig 1 | grep "Logical devices\/Failed\/Degraded" | cut -f2 -d":" | cut -f3 -d"/"
+  echo 0
 }
 
 # Controller Version Information
 function adpt.bios {
-	$ARCBIN getconfig 1 | grep BIOS | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g'
+  sudo $ARCBIN getconfig 1 | grep BIOS | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g'
 }
 
 function adpt.firm {
-	$ARCBIN getconfig 1 | grep Firmware | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g' | awk 'NR==1'
+  sudo $ARCBIN getconfig 1 | grep Firmware | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g' | awk 'NR==1'
 }
 
 function adpt.driver {
-	$ARCBIN getconfig 1 | grep Driver | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g'
+  sudo $ARCBIN getconfig 1 | grep Driver | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g'
 }
 
 function adpt.flash {
-	$ARCBIN getconfig 1 | grep "Boot Flash" | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g'
+  sudo $ARCBIN getconfig 1 | grep "Boot Flash" | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g'
 }
 
 # Controller Battery Info
 function adpt.battery {
-	$ARCBIN getconfig 1 | grep -A3 "Battery Information" | grep Status | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g'
+  sudo $ARCBIN getconfig 1 | grep -A3 "Battery Information" | grep Status | cut -f2 -d":" | sed -e 's/^ //' | sed -e 's/ /_/g'
 }
 
 # Logical device info
 function adpt.raidlevel {
-	$ARCBIN getconfig 1 | grep "RAID level" | cut -f2 -d":"
+  sudo $ARCBIN getconfig 1 | grep "RAID level" | cut -f2 -d":" | sed 's/ //'
 }
 
 function adpt.raidsize {
-	$ARCBIN getconfig 1 | grep -A6 "Logical device number" | grep Size | cut -f2 -d":" | awk '{print $1}'
+  sudo $ARCBIN getconfig 1 | grep -A6 "Logical device number" | grep Size | cut -f2 -d":" | awk '{print $1}'
 }
 
 # Physical device info
 # 1st device
 function adpt.1state {
-	$ARCBIN getconfig 1 | grep -A5 "Device #0" | grep State | cut -f2 -d":"
+  sudo $ARCBIN getconfig 1 | grep -A5 "Device #0" | grep State | cut -f2 -d":" | sed 's/ //'
 }
 
 function adpt.1power {
-	$ARCBIN getconfig 1 | grep -A18 "Device #0" | grep "Power State" | cut -f2 -d":" | head -n1 | sed -e 's/^ //' | sed -e 's/ /_/g'
+  sudo $ARCBIN getconfig 1 | grep -A18 "Device #0" | grep "Power State" | cut -f2 -d":" | head -n1 | sed -e 's/^ //' | sed -e 's/ /_/g'
 }
 
 function adpt.1smart {
-	$ARCBIN getconfig 1 | grep -A18 "Device #0" | grep "S.M.A.R.T. warning" | cut -f2 -d":"
+  sudo $ARCBIN getconfig 1 | grep -A18 "Device #0" | grep "S.M.A.R.T. warning" | cut -f2 -d":" | sed 's/ //'
 }
 
 # 2nd device
 function adpt.2state {
-	$ARCBIN getconfig 1 | grep -A5 "Device #1" | grep State | cut -f2 -d":"
+  sudo $ARCBIN getconfig 1 | grep -A5 "Device #1" | grep State | cut -f2 -d":" | sed 's/ //'
 }
 
 function adpt.2power {
-	$ARCBIN getconfig 1 | grep -A18 "Device #1" | grep "Power State" | cut -f2 -d":" | head -n1 | sed -e 's/^ //' | sed -e 's/ /_/g'
+  sudo $ARCBIN getconfig 1 | grep -A18 "Device #1" | grep "Power State" | cut -f2 -d":" | head -n1 | sed -e 's/^ //' | sed -e 's/ /_/g'
 }
 
 function adpt.2smart {
-	$ARCBIN getconfig 1 | grep -A18 "Device #1" | grep "S.M.A.R.T. warning" | cut -f2 -d":"
+  sudo $ARCBIN getconfig 1 | grep -A18 "Device #1" | grep "S.M.A.R.T. warning" | cut -f2 -d":" | sed 's/ //'
 }
 
 # Version Info -- Edit this part for your own loss
 #
-function adpt.script.ver {
-	echo ${VERSION}
+function adpt.tribily.ver {
+  echo ${VERSION}
 }
 
 
@@ -108,8 +110,8 @@ if [ ! "$1" ] || [ "$#" -gt "1" ]
 then
         echo "Usage: ./adaptec_status.sh <statusname>"
         echo "Statusnames can be"
-				echo `cat $0 | grep function | grep -v grep | awk '{print $2}' | grep -v Run`
-        exit 0  
+        echo `cat $0 | grep function | grep -v grep | awk '{print $2}' | grep -v Run`
+        exit 0
 fi
 
 
